3fce2372749c2a6a40f9ffb4f0f6d9a21d48e9cd
AzadXx77 <cse2021100@rcciit.org.in>
2025-02-10 22:35:55 +0530

    Update ApiService.cs

--- frontend/src/Services/ApiService.cs
+++ frontend/src/Services/ApiService.cs
@@ -1,6 +1,7 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Collections.Generic; // Added for List<T>
 
 public class ApiService
 {
@@ -15,5 +16,35 @@ public class ApiService
     {
         var response = await _httpClient.GetStringAsync(url);
         return JsonConvert.DeserializeObject<T>(response);
+    }
+
+    public async Task<List<T>> GetListAsync<T>(string url) // Added Component 1
+    {
+        var response = await _httpClient.GetStringAsync(url);
+        return JsonConvert.DeserializeObject<List<T>>(response);
+    }
+
+    public async Task<HttpResponseMessage> PostAsync<T>(string url, T data) // Added Component 2
+    {
+        var json = JsonConvert.SerializeObject(data);
+        var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+        return await _httpClient.PostAsync(url, content);
+    }
+
+    public async Task<HttpResponseMessage> PutAsync<T>(string url, T data) // Added Component 3
+    {
+        var json = JsonConvert.SerializeObject(data);
+        var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+        return await _httpClient.PutAsync(url, content);
+    }
+
+    public async Task<HttpResponseMessage> DeleteAsync(string url) // Added Component 4
+    {
+        return await _httpClient.DeleteAsync(url);
+    }
+
+    public async Task<string> GetRawAsync(string url) // Added Component 5
+    {
+        return await _httpClient.GetStringAsync(url);
     }
 }
